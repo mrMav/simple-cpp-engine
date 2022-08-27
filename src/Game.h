065de0c9ee64d9493206825c733744442af6cc3e
mrmav <mrmav@outlook.pt>
2022-08-27 14:49:45 +0100

    base game class

--- src/Game.h
+++ src/Game.h
@@ -1,14 +1,35 @@
+#pragma once
 
-class Game
+#include <GLFW/glfw3.h>
+
+namespace Engine
 {
-    public:
 
-        void Init();
-        void Update(float time);
-        void Render(float time);
-        void Shutdown();
+    class Game
+    {
+        public:
 
-    private:
+            Game(uint32_t screenWith, uint32_t screenHeight, char* windowTitle);
+            ~Game();
 
-        float m_SomeValue;
-};
+            void Run();
+            void Update(float delta);
+            void Render(float delta);
+            void Shutdown();
+
+            uint32_t GetScreenWidth()  { return m_screenWidth;  }
+            uint32_t GetScreenHeight() { return m_screenHeight; }
+
+        private:
+            
+            void Init();
+
+            uint32_t m_screenWidth;
+            uint32_t m_screenHeight;
+
+            GLFWwindow* m_windowHandle;
+            std::string m_windowTitle;
+
+    };
+
+}
